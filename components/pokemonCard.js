cf29b505c740edcea1f361687597bc8226f7e58d
Maximilien <maximilien.lemoine.pro@gmail.com>
2024-01-21 18:01:13 +0100

    feat : capitalize

--- components/pokemonCard.js
+++ components/pokemonCard.js
@@ -1,15 +1,26 @@
 import {View, Image, Text, StyleSheet, TouchableOpacity} from "react-native";
 import React from "react";
+import {useNavigation} from "@react-navigation/native";
 
 export default function PokemonCard({name, url}) {
+    const navigation = useNavigation();
     const id = url.split('/')[url.split('/').length - 2];
     const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
 
+    function capitalizeFirstLetter(string) {
+        if (string === undefined)
+            return '';
+
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
     return (
-        <TouchableOpacity style={styles.card}>
+        <TouchableOpacity onPress={() => navigation.navigate('Pokemon', {
+            id: id
+        })} style={styles.card}>
             <Image style={styles.image} source={{uri: image}}/>
             <View style={styles.titleDiv}>
-                <Text style={styles.title}>{name}</Text>
+                <Text style={styles.title}>{capitalizeFirstLetter(name)}</Text>
             </View>
         </TouchableOpacity>
     )
